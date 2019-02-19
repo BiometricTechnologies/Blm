853c2e62d74064c9331a157897078ad7f198fac7
Alexander Ordin <ordin@reksoft.ru>
2019-02-20 00:08:49 +0300

    Added libs

---
+++ indss_matching_service_solution/IntegrationTests/UnitTest1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using IdentaZone.Indss.MatchingService;
+using System.ServiceModel;
+using System.ServiceModel.Description;
+using System.ServiceModel.Web;
+
+namespace IntegrationTests
+{
+    [TestClass]
+    public class UnitTest1
+    {/*
+        static MatchingServiceHost matchingService;
+
+        [ClassInitialize]
+        public static void Init(TestContext context)
+        {
+            matchingService = new MatchingServiceHost();
+            
+        }
+
+        [TestMethod]
+        public void CheckEcho()
+        {
+            using (ChannelFactory<MatchingServiceInterfaceChannel> scf = new ChannelFactory<MatchingServiceInterfaceChannel>(new BasicHttpBinding(), "http://localhost:8001/MatchingService/Soap"))
+            {
+                try
+                {
+                    MatchingServiceInterfaceChannel channel = scf.CreateChannel();
+
+                    Console.WriteLine("Calling EchoWithGet on SOAP endpoint: ");
+                    var respone = channel.Echo(new EchoRequest("Hello, world"));
+                    Assert.AreEqual("Hello, world", respone.output);
+                }
+                catch (Exception ex)
+                {
+                    System.Console.WriteLine(ex);
+                    Assert.Fail(ex.ToString());
+                }
+            }
+        }*/
+    }
+}
